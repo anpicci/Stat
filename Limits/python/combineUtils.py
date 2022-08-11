450b51e54f6a57a3e0741f29bd01e95060946e49
apiccine <andrea.piccinelli@cern.ch>
2022-08-11 23:24:05 +0200

    update

--- Limits/python/combineUtils.py
+++ Limits/python/combineUtils.py
@@ -121,25 +121,31 @@ def runSinglePointVBS_EWvsQCD(path_, model, categories, method, runSingleCat, ye
                 intervalstr = ""
                 valuestr = ""
                 for idvm, vbsmodel in enumerate(vbsmodels):
-                    cmd += "--PO 'map=.*/" + vbsmodel + ":k_" + vbsmodel.split("_")[-1] + "[1,-150.0,150.0]' " 
+                    if "_DNN_" in path_:
+                        cmd += "--PO 'map=.*/" + vbsmodel + ":k_" + vbsmodel.split("_")[-1] + "[1,-150.0,150.0]' "
+                    else:
+                        cmd += "--PO 'map=.*/" + vbsmodel + ":k_" + vbsmodel.split("_")[-1] + "[1,-200.0,200.0]' "
                     if idvm > 0:
                         modComb += ","
                         intervalstr += ":"
                         valuestr += ","
                     modComb += "k_" + vbsmodel.split("_")[-1]
-                    intervalstr += "k_" + vbsmodel.split("_")[-1] + "=-150.0,150.0" 
+                    if "_DNN_" in path_:
+                        intervalstr += "k_" + vbsmodel.split("_")[-1] + "=-150.0,150.0"
+                    else:
+                        intervalstr += "k_" + vbsmodel.split("_")[-1] + "=-200.0,200.0"
                     valuestr += "k_" + vbsmodel.split("_")[-1] + "=1" 
                 cmd += global_dc + " -o " + rootdc 
                 print cmd
                 os.system(cmd)
             
-                cmd = "$CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/parallelScan.py " + rootdc + " -M MultiDimFit -m 125 -t -1 --redefineSignalPOIs " + modComb + " --setParameters " + valuestr + " --setParameterRanges " + intervalstr + " " + optionalsSM # + " --freezeParameters r --setParameters r=1"
+                cmd = "$CMSSW_BASE/src/HiggsAnalysis/CombinedLimit/test/parallelScan.py " + rootdc + " -M MultiDimFit -m 125 -t -1 --redefineSignalPOIs " + modComb + " --setParameterRanges " + intervalstr + " --autoBoundsPOIs " + modComb + " --autoRange 5 " + optionalsSM # " --setParameters " + valuestr + " --freezeParameters r --setParameters r=1"
                 cmd += " ; hadd -f higgsCombineTest.MultiDimFit.mH125.root higgsCombineTest.*.MultiDimFit.mH125.root"
             
                 print cmd
                 runCombine(cmd, "ls_k_" + model[0] + "_" + method + ".log")
                 
-                cmd = "python " + maindir + "drawLS.py --in0 higgsCombine" + modelname + ".MultiDimFit.mH125.root --in1 higgsCombine" + modelname + ".MultiDimFit.mH125.root --coeff EWK:QCD " 
+                cmd = "python " + maindir + "drawLS.py --in0 higgsCombineTest.MultiDimFit.mH125.root --in1 higgsCombineTest.MultiDimFit.mH125.root --coeff EWK:QCD " 
                 cmd += " --2D"
                 cmd += " --year " 
                 for year in years:
