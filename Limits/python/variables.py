e79e4c571bd460a9bc47c71aec51ed4fad5f9cf0
apiccine <andrea.piccinelli@cern.ch>
2022-11-23 10:36:09 +0100

    results updated for 050

--- Limits/python/variables.py
+++ Limits/python/variables.py
@@ -40,15 +40,30 @@ nbin_bdtsm_dev = len(bin_bdtsm_dev) - 1
 variables.append(variabile('DNN_SM_final_1', 'SM DNN output (final 1)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (final)"))
 variables.append(variabile('DNN_SM_rec', 'SM DNN output (reco)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (reco)"))
 
-variables.append(variabile('DNN_dim6_final_2', 'dim6 DNN output (final 2)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim6 LIN+QUAD"))
-variables.append(variabile('DNN_dim8_final_3', 'dim8 DNN output (final 3)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 LIN+QUAD f3"))
-variables.append(variabile('DNN_dim8_final_3_1to2', 'dim8 DNN output (final 3 1to2)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 LIN+QUAD f3 1to2"))
-variables.append(variabile('DNN_dim8_final_3_again', 'dim8 DNN output (final 3)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 LIN+QUAD f3 again"))
+variables.append(variabile('DNN_dim6_final_2', 'dim6 DNN output (final 2)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim6"))
+variables.append(variabile('DNN_dim8_final_3', 'dim8 DNN output (final 3)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 f3"))
+variables.append(variabile('DNN_dim8_final_3_1to2', 'dim8 DNN output (final 3 1to2)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 f3 1to2"))
+variables.append(variabile('DNN_dim8_final_3_again', 'dim8 DNN output (final 3)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 f3 again"))
 variables.append(variabile('DNN_dim6_final_2_noQUAD', 'dim6 DNN output (final 2 noQUAD)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim6 LIN"))
 variables.append(variabile('DNN_dim8_final_3_noQUAD_fix', 'dim8 DNN output (final 3 noQUAD)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 LIN"))
 variables.append(variabile('DNN_dim8_final_3_NOMOREDY_test', 'dim8 DNN output (f3 NMR test)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "dim8 f3 NMR test"))
 
+
 variables.append(variabile('DNN_SM_final_1_NOMOREDY_test', 'SM DNN output (f1 NMR test)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (f1 NMR test)"))
+variables.append(variabile('DNN_dim8_final_3_NOMOREDY_test', 'dim8 DNN output (f3 NMR test)', True, nbin_bdtsm_dev, bin_bdtsm_dev, "dim8 DNN f3 NMR test"))
+#variables.append(variabile('DNN_dim6_final_2_NOMOREDY_test', 'dim6 DNN output (f2 NMR test)', True, nbin_bdtsm_dev, bin_bdtsm_dev, "dim6 DNN f2 NMR test"))
+
+
+
+variables.append(variabile('DNN_SM_final_1_NOMOREDY_lower', 'SM DNN output (lower LR)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN  (f1 NMR lower)"))
+variables.append(variabile('DNN_SM_final_1_NOMOREDY_lower_4000', 'SM DNN output (lower LR 4000)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (f1 NMR lower 4000)"))
+variables.append(variabile('DNN_SM_final_1_NOMOREDY_lower_4001', 'SM DNN output (lower LR 4001)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (f1 NMR lower 4001)"))
+variables.append(variabile('DNN_SM_final_1_NOMOREDY_lower_bisnotopt', 'SM DNN output (lower LR bis)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (f1 NMR lower bis)"))
+variables.append(variabile('DNN_SM_final_1_NOMOREDY_lower_bisnotopt_4000', 'SM DNN output (lower LR bis 4000)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (f1 NMR lower bis 4000)"))
+variables.append(variabile('DNN_SM_final_1_NOMOREDY_lower_bisnotopt_4001', 'SM DNN output (lower LR bis 4001)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "SM DNN (f1 NMR lower bis 4001)"))
+variables.append(variabile('DNN_dim6_final_2_NOMOREDY_test_nodivide', 'dim6 DNN output (f2 NMR test nodivide)', True, nbin_bdtsm_dev, bin_bdtsm_dev, "dim6 f2 NMR test nodivide"))
+variables.append(variabile('DNN_dim6_final_2_NOMOREDY_lower_halfway', 'dim6 DNN output (f2 NMR lower halfway)', True, nbin_bdtsm_dev, bin_bdtsm_dev, "dim6 f2 NMR lower hw"))
+variables.append(variabile('DNN_dim8_final_3_NOMOREDY_lower', 'dim8 DNN output (f3 NMR lower)', True, nbin_bdtsm_dev, bin_bdtsm_dev, "dim8 f3 NMR lower"))
 
 
 #variables.append(variabile('DNN_pol_final_1', 'pol DNN output (final 1)', True, nbin_bdtsm_dev, bin_bdtsm_dev, smtitle = "pol DNN (final)"))
--- Limits/test/ciplots.py
+++ Limits/test/ciplots.py
@@ -266,7 +266,7 @@ for srv, crv in varloops:
 
 y = []
 
-outfolder = "CIplots_" + pdftype + "_DYinOS_oneIFSR_" + opt.folder + "/" + opt.tagfold
+outfolder = "CIplots_prova_" + pdftype + "_DYinOS_oneIFSR_" + opt.folder + "/" + opt.tagfold
 if opt.wfc:
     outfolder += "_WithFakeCR"
 if opt.pdfttdy:
