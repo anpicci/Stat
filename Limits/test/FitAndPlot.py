58be1765de19aef1b2890ecc1d31d51bef02514d
apiccine <andrea.piccinelli@cern.ch>
2023-03-08 08:24:06 +0100

    results updated for 055

--- Limits/test/FitAndPlot.py
+++ Limits/test/FitAndPlot.py
@@ -117,7 +117,8 @@ for model in models:
     
         #fitfolder = "FitResults_" + folder
         #fitfolder = "FITRESULTS_" + folder
-        fitfolder = "FIT_cons_" + folder
+        #fitfolder = "FIT_cons_" + folder
+        fitfolder = "FIT_fstats_" + folder
         #fitfolder = "FIT_" + folder
         #fitfolder = "FITPROVA_" + folder
         if opt.unblind:
